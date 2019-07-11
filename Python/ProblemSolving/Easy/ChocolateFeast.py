108272dbe364e5b76754ea9295388fde5f504ada
Paradiddle131 <ebhan96@hotmail.com>
2019-07-11 12:53:49 +0300

    Chocolate Feast Solved (Python)

--- Python/ProblemSolving/Easy/ChocolateFeast.py
+++ Python/ProblemSolving/Easy/ChocolateFeast.py
@@ -1,11 +1,14 @@
 def chocolateFeast(n, c, m):
     count = 0
     wrapper = n / c
-    newChocolate = 0
-    while wrapper > m:
+    while wrapper >= m:
         newChocolate, wrapper = divmod(wrapper, m)
         wrapper += newChocolate
-    return n/c + newChocolate
+        count += newChocolate
+    return int(count + n/c)
 
 
 print(chocolateFeast(15, 3, 2))  # 9 bars
+print(chocolateFeast(10, 2, 5))  # 6 bars
+print(chocolateFeast(12, 4, 4))  # 3 bars
+print(chocolateFeast(6, 2, 2))  # 5 bars
